c2668e0d4efba85c69c2588f55192f6ea3536c9a
Wesley Rocha <wesleysr1997@gmail.com>
2020-08-15 10:30:02 -0400

    Clean and organize default pages

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>DOJO Online Judge Orchestrator</title>
+        <link rel="stylesheet" href="/favicon.ico" />
+      </Head>
+      <main>DOJO Online Judge Orchestrator</main>
+    </div>
+  );
+}
