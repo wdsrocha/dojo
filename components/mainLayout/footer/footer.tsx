c5c5c8f87ab2cb6b0edaf87f93f2bdbb9d98553f
Wesley Rocha <wesleysr1997@gmail.com>
2020-08-16 00:00:42 -0400

    Nit to trigger Vercel 😉

--- components/mainLayout/footer/footer.tsx
+++ components/mainLayout/footer/footer.tsx
@@ -3,7 +3,7 @@ import { Layout } from "antd";
 
 const Footer: FC = () => (
   <Layout.Footer className="text-center">
-    DOJO ©2020 Created by Wesley Rocha
+    DOJO © 2020 Created by Wesley Rocha
   </Layout.Footer>
 );
 
