fe02b338fbd242c54e5269ef1290f6f30b73d7c3
Wesley Rocha <wesleysr1997@gmail.com>
2020-08-15 23:39:07 -0400

    Add lorem ipsum temporary text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,53 @@
 import React, { FC } from "react";
 
-const Home: FC = () => <div className="bg-white p-2 h-64" />;
+const Home: FC = () => (
+  <div className="bg-white px-4 pt-4 pb-8">
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam pariatur
+      eius, praesentium omnis ullam expedita laboriosam ipsum, magni, earum
+      perspiciatis quod ipsam! Mollitia non ex repellendus tenetur temporibus,
+      quibusdam iste.
+    </p>
+    <br />
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta, sapiente!
+      Expedita quod nulla commodi libero perferendis alias magni ad similique
+      veritatis est repellendus facere incidunt, numquam laborum ratione
+      consectetur consequuntur error itaque sint odit dolores. Dolore animi ad
+      aut quae asperiores esse maxime autem officiis, ratione sunt tenetur dicta
+      tempora voluptatum incidunt sit saepe doloribus id magni corporis nemo
+      consequatur quaerat quibusdam alias! Perferendis porro animi neque vitae
+      laudantium dolorem! Tenetur magni recusandae quia, animi velit delectus
+      labore? Dolor sed quos, sunt eos quaerat minima officiis cum nemo magnam
+      asperiores! Assumenda corporis repellendus molestiae placeat atque! Neque
+      consequuntur minus laudantium atque quis vero ut veritatis ex! Modi sint
+      pariatur in laborum voluptas, exercitationem non. Saepe obcaecati,
+      deleniti perspiciatis temporibus quo commodi vero sunt quis reiciendis
+      assumenda explicabo nisi quibusdam, aliquam nam eveniet nulla minima
+      incidunt doloribus pariatur quod id dolorum! Dignissimos quo dolores
+      reiciendis blanditiis tempore voluptatibus! Fuga asperiores quasi, dolores
+      sed in dicta suscipit quisquam tempora? Dolorem et architecto similique,
+      velit quam explicabo adipisci porro placeat temporibus libero unde sit
+      minus quia soluta repudiandae fugiat sint ipsam laudantium, aperiam culpa
+      facere est optio? Consequatur ipsam necessitatibus dolorum id et aperiam
+      officia alias ab sint minus exercitationem quod, aut maiores, neque hic
+      iste quo perspiciatis explicabo qui, voluptas incidunt perferendis
+      cupiditate. Veritatis assumenda doloribus itaque necessitatibus est
+      adipisci consequuntur modi facere id, voluptas aperiam reiciendis nisi
+      voluptatem omnis placeat magni, dolore consequatur culpa quasi eligendi,
+      quia corrupti! Molestias, voluptatem. Eos debitis, quia pariatur quas
+      quae, non nam modi illum numquam sapiente recusandae rerum unde, quis
+      labore.
+    </p>
+    <br />
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero quibusdam
+      saepe ipsa, laborum sint et est officiis, tempora ut nesciunt molestias?
+      Quas culpa error sint id quidem aperiam, nostrum fugiat?
+    </p>
+    <br />
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+  </div>
+);
 
 export default Home;
