f5c4ce0542e067aaac51ed5ac6e8f93c2283550b
Wesley Rocha <wesleysr1997@gmail.com>
2020-08-29 14:13:08 -0400

    Set locale to ptBR
    This is temporary

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { AppProps } from "next/app";
+import { ConfigProvider } from "antd";
+import ptBR from "antd/lib/locale/pt_BR";
 
 import MainLayout from "../components/mainLayout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MainLayout>
-      <Component {...pageProps} />
-    </MainLayout>
+    <ConfigProvider locale={ptBR}>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </ConfigProvider>
   );
 }
 
