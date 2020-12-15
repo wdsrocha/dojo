83c2dcb13627bc310385de61381ec80f0a3a4bd7
Wesley Rocha <wesleysr1997@gmail.com>
2020-12-14 23:27:36 -0400

    Add tests for URI adapter

---
+++ packages/server/src/__tests__/puppeteer.mock.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Browser, Page, NavigationOptions, ClickOptions } from 'puppeteer';
+
+export const stubPage = ({
+  goto: (_url: string) => Promise.resolve(),
+  $: (_selector: string) => Promise.resolve(),
+  type: (_selector: string, _text: string) => Promise.resolve(),
+  click: (_selector: string, _clickOptions: ClickOptions) => Promise.resolve(),
+  waitForNavigation: (_navigationOptions: NavigationOptions) =>
+    Promise.resolve(),
+  url: (): Promise<string> => Promise.resolve(''),
+} as unknown) as Page;
+
+export const stubBrowser = ({
+  newPage: () => Promise.resolve(stubPage),
+  close: () => Promise.resolve(),
+} as unknown) as Browser;
+
+export const stubPuppeteer = ({
+  launch: () => Promise.resolve(stubBrowser),
+} as unknown) as any;
