8ca6548933f6b5e4613194def18eccebd59a53ea
Wesley Rocha <wesleysr1997@gmail.com>
2020-12-20 23:50:02 -0400

    Run linter with import rules

--- packages/server/src/online-judges/online-judges.module.ts
+++ packages/server/src/online-judges/online-judges.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+
 import { UriAdapter } from './adapters/uri/uri-adapter';
 import { UriModule } from './adapters/uri/uri.module';
 import { OnlineJudgesService } from './online-judges.service';
--- packages/server/src/submissions/submissions.module.ts
+++ packages/server/src/submissions/submissions.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
-import { UriAdapter } from 'src/online-judges/adapters/uri/uri-adapter';
-import { OnlineJudgesService } from 'src/online-judges/online-judges.service';
+
+import { UriAdapter } from '../online-judges/adapters/uri/uri-adapter';
+import { OnlineJudgesService } from '../online-judges/online-judges.service';
 import { SubmissionsController } from './submissions.controller';
 import { SubmissionsService } from './submissions.service';
 
