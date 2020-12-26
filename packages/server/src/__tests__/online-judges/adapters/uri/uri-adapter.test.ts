88b4d284f44f7604d50c47f34896a442a5d6cccc
Wesley Rocha <wesleysr1997@gmail.com>
2020-12-25 20:58:15 -0400

    Refactor tests

--- packages/server/src/__tests__/online-judges/adapters/uri/uri-adapter.test.ts
+++ packages/server/src/__tests__/online-judges/adapters/uri/uri-adapter.test.ts
@@ -13,55 +13,39 @@ jest.mock('puppeteer', () => ({
   },
 }));
 
-beforeEach(() => {
-  jest.restoreAllMocks();
+process.env.URI_BOT_EMAIL = 'email';
+process.env.URI_BOT_PASSWORD = 'password';
+
+it('should get the problem title', async () => {
+  const uriAdapter = new UriAdapter();
+  fetch.mockReturnValue(Promise.resolve(new Response(problemPage)));
+  const { title } = await uriAdapter.getProblem('1001');
+  expect(title).toBe('Extremamente Básico');
 });
 
-describe('UriAdapter', () => {
-  const OLD_ENV = process.env;
+it('should throw 404 error when getting the problem title for an unknown problem', async () => {
+  const uriAdapter = new UriAdapter();
+  fetch.mockReturnValue(Promise.resolve(new Response(problemPageNotFound)));
+  const { title } = await uriAdapter.getProblem('999');
+  expect(title).toBeFalsy();
+});
 
-  beforeEach(() => {
-    jest.resetModules();
-    process.env = { ...OLD_ENV };
-    process.env.URI_BOT_EMAIL = 'email';
-    process.env.URI_BOT_PASSWORD = 'password';
-  });
+it('should submit a problem and receive the submission id', async () => {
+  const uriAdapter = new UriAdapter();
 
-  afterAll(() => {
-    process.env = OLD_ENV;
-  });
-
-  it('should get the problem title', async () => {
-    const uriAdapter = new UriAdapter();
-    fetch.mockReturnValue(Promise.resolve(new Response(problemPage)));
-    const { title } = await uriAdapter.getProblem('1001');
-    expect(title).toBe('Extremamente Básico');
-  });
-
-  it('should throw 404 error when getting the problem title for an unknown problem', async () => {
-    const uriAdapter = new UriAdapter();
-    fetch.mockReturnValue(Promise.resolve(new Response(problemPageNotFound)));
-    const { title } = await uriAdapter.getProblem('999');
-    expect(title).toBeFalsy();
-  });
-
-  it('should submit a problem and receive the submission id', async () => {
-    const uriAdapter = new UriAdapter();
-
-    jest
-      .spyOn(stubPage, 'url')
-      .mockReturnValue(
-        await Promise.resolve(
-          'https://www.urionlinejudge.com.br/judge/pt/runs/code/20596989',
-        ),
-      );
-
-    const { submissionId } = await uriAdapter.submit(
-      '1000',
-      '20',
-      'print("Hello World!")',
+  jest
+    .spyOn(stubPage, 'url')
+    .mockReturnValue(
+      await Promise.resolve(
+        'https://www.urionlinejudge.com.br/judge/pt/runs/code/20596989',
+      ),
     );
 
-    expect(submissionId).toBe('20596989');
-  });
+  const { submissionId } = await uriAdapter.submit(
+    '1000',
+    '20',
+    'print("Hello World!")',
+  );
+
+  expect(submissionId).toBe('20596989');
 });
