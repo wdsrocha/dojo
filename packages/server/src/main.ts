847be26bd34207968d5d0bdbc58d85df909392a7
Wesley Rocha <wesleysr1997@gmail.com>
2020-12-27 03:48:46 -0400

    Add validation pipe

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 
@@ -5,6 +6,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   await app.listen(2000);
 }
