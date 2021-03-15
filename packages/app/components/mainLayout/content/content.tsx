31ed1d268ad5a3b4d04196f0ba8024c7fc7cd5df
Wesley Rocha <wesleysr1997@gmail.com>
2021-03-15 05:22:36 -0400

    Horizontally centralize pages

--- packages/app/components/mainLayout/content/content.tsx
+++ packages/app/components/mainLayout/content/content.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import { Layout } from "antd";
 
 const Content: FC = ({ children }) => (
-  <Layout.Content className="md:px-16 px-6 pt-6">{children}</Layout.Content>
+  <Layout.Content style={{ width: '100%', maxWidth: '1200px', margin: '24px auto 16px' }}>{children}</Layout.Content>
 );
 
 export default Content;
