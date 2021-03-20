d0f6c0a9edcdd49438a47c460fd08bb293a3cf31
Wesley Rocha <wesleysr1997@gmail.com>
2021-03-20 14:14:04 -0400

    Add loader for client side routing

--- packages/app/pages/_app.tsx
+++ packages/app/pages/_app.tsx
@@ -1,11 +1,18 @@
 import { AppProps } from "next/app";
 import { ConfigProvider } from "antd";
 import ptBR from "antd/lib/locale/pt_BR";
+import { start, done } from "nprogress";
+import 'nprogress/nprogress.css';
+import { Router } from "next/dist/client/router";
 
 import MainLayout from "../components/mainLayout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  Router.events.on("routeChangeStart", start);
+  Router.events.on("routeChangeComplete", done);
+  Router.events.on("routeChangeError", done);
+
   return (
     <ConfigProvider locale={ptBR}>
       <MainLayout>
