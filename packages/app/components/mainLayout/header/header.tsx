45128e205740c2193f573128a50b7a9c06c173ac
Wesley Rocha <wesleysr1997@gmail.com>
2021-05-16 06:05:49 -0400

    Centralize header

--- packages/app/components/mainLayout/header/header.tsx
+++ packages/app/components/mainLayout/header/header.tsx
@@ -10,37 +10,42 @@ export const Header = () => {
   const [isDrawerVisible, setIsDrawerVisible] = useState<boolean>(false);
 
   return (
-    <Layout.Header className="flex items-center justify-between md:px-16 px-6">
-      <div className="mr-6 flex flex-1">
-        <Link href="/">
-          <a className="flex text-3xl font-bold text-white">
-            <span role="img" aria-label="DOJO Logo">
-              🥋
-            </span>
-            <span className="ml-2 text-lg">DOJO</span>
-          </a>
-        </Link>
-      </div>
-      <nav className="hidden md:flex md:items-center md:justify-between md:w-full">
-        <LeftMenu mode="horizontal" theme="dark" />
-        <RightMenu mode="horizontal" theme="dark" />
-      </nav>
-      <Button
-        className="md:hidden flex items-center justify-center"
-        type="primary"
-        icon={<MenuOutlined />}
-        onClick={() => setIsDrawerVisible(true)}
-      />
-      <Drawer
-        title="Menu"
-        placement="right"
-        closable={false}
-        onClose={() => setIsDrawerVisible(false)}
-        visible={isDrawerVisible}
+    <Layout.Header className="flex justify-center px-0">
+      <div
+        className="flex items-center"
+        style={{ width: "100%", maxWidth: "1200px" }}
       >
-        <LeftMenu mode="inline" />
-        <RightMenu mode="inline" />
-      </Drawer>
+        <div className="mx-5 flex flex-1">
+          <Link href="/">
+            <a className="flex text-3xl font-bold text-white">
+              <span role="img" aria-label="DOJO Logo">
+                🥋
+              </span>
+              <span className="ml-2 text-lg">DOJO</span>
+            </a>
+          </Link>
+        </div>
+        <nav className="hidden md:flex md:items-center md:justify-between md:w-full">
+          <LeftMenu mode="horizontal" theme="dark" />
+          <RightMenu mode="horizontal" theme="dark" />
+        </nav>
+        <Button
+          className="md:hidden flex items-center justify-center mr-5"
+          type="primary"
+          icon={<MenuOutlined />}
+          onClick={() => setIsDrawerVisible(true)}
+        />
+        <Drawer
+          title="Menu"
+          placement="right"
+          closable={false}
+          onClose={() => setIsDrawerVisible(false)}
+          visible={isDrawerVisible}
+        >
+          <LeftMenu mode="inline" />
+          <RightMenu mode="inline" />
+        </Drawer>
+      </div>
     </Layout.Header>
   );
 };
